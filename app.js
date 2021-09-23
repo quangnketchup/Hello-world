69a0b8357421527d7ed7447d1b073e4f7220662a
quangnketchup <90783531+quangnketchup@users.noreply.github.com>
2021-09-23 22:51:07 +0700

    Add files via upload

--- app.js
+++ app.js
@@ -1,3 +1,11 @@
-var msg= 'Hello wrold';
-console.log(msg);
-console.log("NguyenVanQuang")
+const express = require('express')
+const app = express()
+const port = 3333
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
